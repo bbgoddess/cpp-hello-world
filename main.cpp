23f2250b7e6190859306aa3875f85d18f268241f
Boushra Bettir <boushra.bettir04@csu.fullerton.edu>
2022-09-18 19:39:05 -0700

    BTS

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
-using namespace std;
+//using namespace std;
 
-int main()
-{   
-    cout << "Hello World!" << endl;
-    return 0;
+int main(int argc, char const *argv[]) {
+  
+  std::cout << "I love BTS" << '\n'; 
+  return 0;
+
 }
+
+
+
+
